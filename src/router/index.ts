ffc6166936348fecf74c31f503eb20a9515e06ff
Stefano Imanol <stefano.ramirez.u@uni.pe>
2023-12-08 22:31:51 -0500

    add

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,20 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (Home-[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import('@/views/Home.vue'),
-      },
-    ],
-  },
+  
   {
     path: '/InspeccionMuestreoAceite',
     component: () => import('@/layouts/default/Default.vue'),
@@ -87,7 +74,7 @@ const routes = [
     ],
   },
   {
-    path: '/Servicios',
+    path: '/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
